2bf9d6d7f5bb02308c5b65000c57216c44bbcc24
qxan <mehmet.enfa@gmail.com>
2023-04-22 23:44:39 +0300

    _app

--- pages/blog/[id].js
+++ pages/blog/[id].js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { blogdata } from '@/assets/data/dummydata'
+import Banner from '@/components/Banner'
+import { Title, TitleSm } from '@/components/common/Title'
+import Head from 'next/head'
+import { useRouter } from 'next/router'
 
-const [id] = () => {
+const SinglePost = () => {
+  const router = useRouter()
+  const { id } = router.query
+  const post = blogdata.find((post) => post.id === parseInt(id))
+
   return (
-    <div>[id]</div>
+    <>
+      <Head>
+        <title>{post.title}</title>
+      </Head>
+    </>
   )
 }
 
-export default [id]
+export default SinglePost
