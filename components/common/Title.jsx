f9b534b74894391bc5c40e5907017ec6e05ba550
qxan <mehmet.enfa@gmail.com>
2023-04-24 18:49:12 +0300

    title file

--- components/common/Title.jsx
+++ components/common/Title.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
-
-const Title = () => {
+export const TitleLogo = ({ title, caption, className }) => {
   return (
-    <div>Title</div>
-  )
-}
+    <h1 className={`${className} title-logo`}>
+      <span>{caption}</span>
+      {title}
+    </h1>
+  );
+};
 
-export default Title
+export const TitleSm = ({ title }) => {
+  return <h1 className="titleSm">{title}</h1>;
+};
+
+export const Title = ({ title, className }) => {
+  return <h1 className={`${className} title`}>{title}</h1>;
+};
