010c22580e4ef6024c44b4884f880532eeb4724b
qxan <mehmet.enfa@gmail.com>
2023-04-25 23:41:37 +0300

    banner

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Title, TitleLogo } from "./common/Title";
 
 const Banner = () => {
   return (
-    <div>Banner</div>
-  )
-}
+    <>
+      <section className="banner">
+        <div className="container">
+          <div>
+            <Title title="We are looking forward to start a new project" />{" "}
+            <br />
+            <TitleLogo title="Lets take your business to the next level!" />
+          </div>
+          <div>
+            <button className="button-primary">Request a call-back</button>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Banner
+export default Banner;
