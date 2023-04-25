7f34bb58b75fedacea6112d635a492dbded74058
qxan <mehmet.enfa@gmail.com>
2023-04-25 18:59:03 +0300

    cardFile

--- components/common/Card.jsx
+++ components/common/Card.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import Link from "next/link";
+import { TitleSm } from "./Title";
+import { HiOutlineArrowRight } from "react-icons/hi";
 
-const Card = () => {
+export const Card = ({ data, caption, show, path }) => {
   return (
-    <div>Card</div>
-  )
-}
+    <>
+      <div className="card">
+        <div className="card-img">
+          <img src={data.cover} alt={data.title} />
+        </div>
+        <div className="card-details">
+          <Link href={`${path}/${data.id}`} className="title-link">
+            <TitleSm title={data.title} />
+          </Link>
+          {caption && (
+            <Link href={`${path}/${data.id}`}>
+              {caption} <HiOutlineArrowRight className="link-icon" />
+            </Link>
+          )}
+          <div className="flex">
+            <span> {data.catgeory} </span>{" "}
+            {data.date && <span> / {data.date}</span>}
+          </div>
 
-export default Card
+          {show && (
+            <ul>
+              {data.desc.map((text, i) => (
+                <li key={i}> - {text.text}</li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
