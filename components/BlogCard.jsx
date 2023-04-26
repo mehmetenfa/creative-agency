88fea13546fd27c7c33c12901078d90bc7041eb2
qxan <mehmet.enfa@gmail.com>
2023-04-26 12:10:26 +0300

    blogCardFile

--- components/BlogCard.jsx
+++ components/BlogCard.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Card } from "./common/Card";
+import { blogdata } from "@/assets/data/dummydata";
 
 const BlogCard = () => {
   return (
-    <div>BlogCard</div>
-  )
-}
+    <>
+      <div className="container blog-card grid-2 py">
+        {blogdata.map((item) => (
+          <Card data={item} key={item.id} path="blogs" />
+        ))}
+      </div>
+    </>
+  );
+};
 
-export default BlogCard
+export default BlogCard;
