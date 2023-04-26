f34378787dedd13e9590dc35902b244101feba38
qxan <mehmet.enfa@gmail.com>
2023-04-26 12:33:55 +0300

    teamPage completed

--- sections/Team.js
+++ sections/Team.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import { teamdata } from '@/assets/data/dummydata'
+import { Card } from '@/components/common/Card'
+import { Title, TitleSm } from '@/components/common/Title'
+import team from '@/pages/team'
 
 const Team = () => {
   return (
-    <div>Team</div>
+    <>
+      <section className='agency bg-top'>
+        <div className='container'>
+          <div className='heading-title'>
+            <TitleSm title='MEET OUR TEAM' /> <br />
+            <br />
+            <Title title='A team of smart & passionate creatives' className='title-bg' />
+          </div>
+          <div className='grid-4 py'>
+            {teamdata.map((item) => (
+              <Card data={item} key={item.id} caption={item.post} />
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
