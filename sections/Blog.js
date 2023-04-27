fdf7d9907bb4d572e1ac3a736175f12010c72bd0
qxan <mehmet.enfa@gmail.com>
2023-04-27 14:59:39 +0300

    blogPage completed

--- sections/Blog.js
+++ sections/Blog.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import { blog } from "@/assets/data/dummydata";
+import BlogCard from "@/components/BlogCard";
+import { Card } from "@/components/common/Card";
+import { Title, TitleSm } from "@/components/common/Title";
+import React from "react";
 
 const Blog = () => {
   return (
-    <div>Blog</div>
-  )
-}
+    <>
+      <section className="agency bg-top">
+        <div className="container">
+          <div className="heading-title">
+            <TitleSm title="BLOG" /> <br />
+            <br />
+            <Title title="Our views on marketing, design & technology" />
+          </div>
+          <BlogCard />
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Blog
+export default Blog;
