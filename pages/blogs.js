bb4ee163aea0e9d2f79b814f57c24852f0a08b18
qxan <mehmet.enfa@gmail.com>
2023-04-27 14:56:18 +0300

    blogPage

--- pages/blogs.js
+++ pages/blogs.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import Head from "next/head";
+import { Blog } from "@/sections";
 
 const blogs = () => {
   return (
-    <div>blogs</div>
-  )
-}
+    <>
+      <Head>
+        <title>Blog - 7Creative</title>
+      </Head>
+      <Blog />
+    </>
+  );
+};
 
-export default blogs
+export default blogs;
