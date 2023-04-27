523ed7fb3decd1cbb4a893ff2ff317ca946846b3
qxan <mehmet.enfa@gmail.com>
2023-04-27 11:41:37 +0300

    servicesPage

--- pages/services.js
+++ pages/services.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
+import Head from "next/head";
+import Services from "@/sections/Services";
 
 const services = () => {
   return (
-    <div>services</div>
-  )
-}
+    <>
+      <Head>
+        <title>Team - 7Creative</title>
+      </Head>
+      <Services />
+    </>
+  );
+};
 
-export default services
+export default services;
